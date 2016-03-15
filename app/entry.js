8ce01d7ee4d0ba1c6751f743cacc87fe3082fc07
Frank Yang <codebear4@gmail.com>
2016-03-15 11:52:29 +0800

    Application menus.
    Signed-off-by: Frank Yang <codebear4@gmail.com>

--- app/entry.js
+++ app/entry.js
@@ -3,9 +3,52 @@
 import electron from 'electron'
 
 const app = electron.app
+const Menu = electron.Menu
 const BrowserWindow = electron.BrowserWindow
 
 let mainWindow
+let menuTemplate = [
+    {
+        label: 'PluginBox',
+        subMenu: [
+            {
+                label: 'About PluginBox',
+                role: 'about'
+            },
+            {
+                label: 'Reload',
+                accelerator: 'CmdOrCtrl+R',
+                click: (item, focus) => {
+                    if (focus) {
+                        focus.reload();
+                    }
+                }
+            },
+            {
+                label: 'Toggle Developer Tools',
+                accelerator: (() => {
+                  if (process.platform == 'darwin')
+                    return 'Alt+Command+I';
+                  else
+                    return 'Ctrl+Shift+I';
+                })(),
+                click: (item, focusedWindow) => {
+                  if (focusedWindow)
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                label: '退出',
+                accelerator: 'Command+Q',
+                click: () => {app.quit()}
+            }
+        ]
+    }
+]
+
+let menu = Menu.buildFromTemplate(menuTemplate)
+Menu.setApplicationMenu(menu)
+
 
 app.on('ready', function() {
     mainWindow = new BrowserWindow({
