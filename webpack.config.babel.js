487816bfdd18a99bb5eb925823a07ffbfad8a502
Frank Yang <codebear4@gmail.com>
2016-03-16 14:20:24 +0800

    Introduce image-loader.
    Signed-off-by: Frank Yang <codebear4@gmail.com>

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -48,6 +48,13 @@ export default {
             {
                 test: /\.svg\??.*$/,
                 loader: "file?name=fonts/[name].[ext]"
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loaders: [
+                    'file?hash=sha512&digest=hex&name=[hash].[ext]',
+                    'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+                ]
             }
         ]
     },
