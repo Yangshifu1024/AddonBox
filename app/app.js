181740fe0847f0787352b09df92fadec0418c196
Frank Yang <codebear4@gmail.com>
2016-03-18 21:04:53 +0800

    We have progress now.
    Signed-off-by: Frank Yang <codebear4@gmail.com>

--- app/app.js
+++ app/app.js
@@ -13,7 +13,8 @@ import VueI18n from 'vue-i18n'
 import setting from './library/setting.js'
 import BigFoot from './provider/bigFoot.js'
 import AddOn from './library/addon.js'
-import axios from 'axios'
+import request from 'request'
+import progress from 'request-progress'
 import * as xml2js from 'xml2js'
 import { progressbar, alert } from 'vue-strap'
 
@@ -43,28 +44,20 @@ new Vue({
         }
     },
     ready() {
-        let progress = (size, total) => {
-            console.log(`Progress: size: ${size} - total: ${total}`)
+        // let progress = (e) => {
+            // console.log(e)
+            // // console.log(`Progress: size: ${size} - total: ${total}`)
+        // }
+        let handler = {}
+        handler.on = (e) => {
+            console.log(e)
         }
-        axios.progress = progress
-        axios.get(BigFoot.indexFileUrl).then(resp => {
-            if (resp.status === 200) {
-                this.isReadyToUpdate = true
-                xml2js.parseString(resp.data, (err, ret) => {
-                    ret.AddOns.AddOn.forEach((value, index) => {
-                        this.addons.push(new AddOn(
-                            index,
-                            value.$.name,
-                            value.$['Title-zhCN'],
-                            value.$['Notes-zhCN'],
-                            value.$.Author,
-                            value.$.Version
-                        ).plain())
-                    })
-                })
-            }
-        }).catch(err => {
-            console.log(err)
+        progress(request(BigFoot.indexFileUrl, (err, resp, body) => {
+            console.log(body)
+        }), {
+            throttle: 0
+        }).on('progress', (e) => {
+            this.progress = e.percentage * 100
         })
     },
     data: {
@@ -77,7 +70,7 @@ new Vue({
         columns: [Vue.t('Addon.Labels.Name'), Vue.t('Addon.Labels.Title'), Vue.t('Addon.Labels.Version')],
         addons: [],
         query: null,
-        progress: 50,
+        progress: 0,
         alerter: {
             show: false,
             style: 'warning',
